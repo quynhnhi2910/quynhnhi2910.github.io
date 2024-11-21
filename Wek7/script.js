46941f2708e7580496aa733c8a62b1dfcb48d498
quynhnhi2910 <quynhnhi.gm2910@gmail.com>
2024-11-21 23:54:48 +0700

    tuan 7

---
+++ Wek7/script.js
@@ -0,0 +1,10 @@
+// function calculate(){
+//     var a =document.getElementById("a_value").value;
+// var result = 1;
+// var output = "Result ";
+// a = parseInt(a);
+// for (var i = 1; i <= a; i++) {
+//     result *= i;
+// }
+// document.getElementById("result").innerHTML=output +result;
+// }
