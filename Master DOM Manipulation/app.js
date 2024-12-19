6c751f951ad36a4ee5497bc84e4db877d6925670
quynhnhi2910 <quynhnhi.gm2910@gmail.com>
2024-12-19 23:09:04 +0700

    .

---
+++ Master DOM Manipulation/app.js
@@ -0,0 +1,19 @@
+// GetElementbyID() 
+const footballElement = document.getElementById('football');
+console.log(footballElement); 
+
+// GetElementbyClassname 
+const divs = document.getElementsByClassName('div1');
+console.log(divs); 
+
+// getElementbyTagname
+const listItems = document.getElementsByTagName('li');
+console.log(listItems); 
+
+// querySelector()
+const firstListItem = document.querySelector('#sports li');
+console.log(firstListItem); 
+// querySelectorAll() 
+const allListItems = document.querySelectorAll('#sports li');
+console.log(allListItems); 
+
